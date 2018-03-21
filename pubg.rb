5a0533623964960d4740e2bdae5595f358600f7c
cocoa1225 <35025624+cocoa1225@users.noreply.github.com>
2018-03-21 19:47:54 +0900

    Update pubg.rb

--- pubg.rb
+++ pubg.rb
@@ -1,80 +1,50 @@
+require 'date'
+require 'twitter'
 require 'open-uri'
 require 'nokogiri'
-require 'discordrb'
-bot = Discordrb::Commands::CommandBot.new token: 'yourtoken', prefix: '!'
 
-bot.command (:pubg) do |event,*code|
-  @name = code[0]
-t = 0
-url = "https://dak.gg/profile/#{@name}"
+puts "実行中！"
+loop do
+  d = DateTime.now
+ hour = d.hour
+ minut = d.min
+ second = d.sec
 
-charset = nil
-html = open(url).read do |f|
-  charset = f.charset # 文字種別を取得
-  f.read # htmlを読み込んで変数htmlに渡す
-end
-# htmlをパース(解析)してオブジェクトを生成
-page = Nokogiri::HTML.parse(html, nil, charset)
-pubgrate = page.search('p.grade-name')
+ if hour == 2 && minut == 14 && second == 0
+   sleep(1)
+  url = "https://pubgtracker.com/profile/pc/cocoaprpr?region=agg"
+  charset = nil
+  html = open(url).read do |f|
+    charset = f.charset
+    f.read
+  end
 
-File.open('file.txt','w') do |f|
-  f.puts("#{pubgrate}")
-end
 
-File.open('./file.txt','r').each do |r|
-bronze = r.include?("BRONZE")
-silver= r.include?("SILVER")
-gold = r.include?("GOLD")
-platinum = r.include?("PLATINUM")
-diamond = r.include?("DIAMOND")
-master = r.include?("MASTER")
-grand = r.include?("GRANDMASTER")
-top500 = r.include?("TOP")
-etc = r.include?("grade-name")
-etc2 = r.include?("/p")
+  page = Nokogiri::HTML.parse(html, nil, charset)
+rating = page.search('span.value')
+  File.open('rating.txt','w') do |f|
+    f.puts("#{rating}")
+ end
 
-if bronze == true
-  event.send_message"#{@name}のレートはブロンズ帯です"
-t +=1
+ File::open("rating.txt") do |content|
+   @tweetrating = content.readlines[18].gsub(/[^\d]/, "").to_i
+ end
 
-elsif silver == true
-  event.send_message"#{@name}のレートはシルバー帯です"
-t +=1
-
-elsif gold == true
-  event.send_message"#{@name}のレートはゴールド帯です"
-t +=1
-
-elsif platinum == true
-  event.send_message"#{@name}のレートはプラチナ帯です"
-t +=1
-
-elsif diamond == true
-  event.send_message"#{@name}のレートはダイアモンド帯です"
-t +=1
-
-elsif grand == true
-  event.send_message"#{@name}のレートはグランドマスター帯です"
-t +=1
-
-elsif top500 == true
-  event.send_message"#{@name}のレートはTOP500位です"
-t +=1
-
-elsif master == true
-  event.send_message"#{@name}のレートはマスター帯です"
-t +=1
-
-elsif etc == true || etc2 == true
-   puts "a"
-t +=1
-
-else
-  event.send_message"ランク帯が取得できなかったか名前が間違っている可能性が高いです"
-  t +=1
-end
-end
-""
+ File.open('hikaku.txt',"r") do |hikaku|
+   @kinou = hikaku.read
 end
 
-bot.run
+File.open('hikaku.txt',"w") do |hikaku2|
+  hikaku2.puts("#{@tweetrating}")
+end
+
+client = Twitter::REST::Client.new do |config|     #ツイートするアカウントの情報を取得
+  config.consumer_key        = "your consumer_key"
+  config.consumer_secret     = "your consumer_secret"
+  config.access_token        = "your access_token"
+  config.access_token_secret = "your access_token_secret"
+end
+
+  puts("あなたの名前のPUBG,Squad(TPP)のレートは#{@tweetrating}です。\n昨日までのレートは#{@kinou}")
+end
+end
